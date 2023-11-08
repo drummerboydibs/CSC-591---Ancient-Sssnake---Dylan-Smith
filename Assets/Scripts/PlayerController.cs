7b5959aa5e06a81f5e03eb70263ec533de8b50e6
Dylan Smith <musicteachersmith@gmail.com>
2023-11-07 22:03:42 -0500

    Early asset setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
+    //private Rigidbody rb;
+    private float speed = 8f;
+    private float turnSpeed = 120f;
+
+    private float attackPower = 50f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+       // rb = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            
-        }
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(0, 0, verticalInput * Time.deltaTime * speed));
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
     }
 }
