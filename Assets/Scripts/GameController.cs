2db8dda5e05e9de46fe45efad2e892823a800fa7
Dylan Smith <musicteachersmith@gmail.com>
2023-11-08 23:38:19 -0500

    Opening scene largely set
    To-Do:
    * UI
    * Keep player snake from tipping over
    * Some scripting for other snakes and sparrow
    * Little more buildings to lead toward temple
    * Set out of bounds (including UI?)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,15 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public int year = 0;
+    public bool isGameOver = false;
+    public int maxSnakes = 6;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,20 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void completeLevel(int year)
+    {
+        if (year == 0)
+        {
+            // load temple scene
+            year++;
+        } else if (year > 0 && year <= maxSnakes)
+        {
+            // increase the snakes' size and bite power
+
+
+            year++;
+        }
     }
 }
