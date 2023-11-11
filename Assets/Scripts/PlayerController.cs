30102771250a90d13e5f2eb99872c81c36d42340
Dylan Smith <musicteachersmith@gmail.com>
2023-11-10 19:44:46 -0500

    Lots of prefab fixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,35 @@ public class PlayerController : MonoBehaviour
     //private Rigidbody rb;
     private float speed = 8f;
     private float turnSpeed = 120f;
+    public float jumpForce = 15f;
 
     private float attackPower = 50f;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-       // rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Control the snake's motion
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(new Vector3(0, 0, verticalInput * Time.deltaTime * speed));
         transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
+
+        // Enable jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+        }
+
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(transform.position.x, 1, transform.position.y);
+        }
     }
 }
